5e9898af89be6a5bf149a8e4462a46d1093cf9ee
Benny Powers <web@bennypowers.com>
2018-09-04 19:49:42 +0300

    haskell: house: passes tests

--- haskell/house/src/House.hs
+++ haskell/house/src/House.hs
@@ -1,19 +1,31 @@
 module House (rhyme) where
 
-finale :: String
-finale = "This is the horse and the hound and the horn\n\
-  \that belonged to the farmer sowing his corn\n\
-  \that kept the rooster that crowed in the morn\n\
-  \that woke the priest all shaven and shorn\n\
-  \that married the man all tattered and torn\n\
-  \that kissed the maiden all forlorn\n\
-  \that milked the cow with the crumpled horn\n\
-  \that tossed the dog\n\
-  \that worried the cat\n\
-  \that killed the rat\n\
-  \that ate the malt\n\
-  \that lay in the house that Jack built.\n"
+import Data.List.Split
 
-rhyme :: String
-rhyme = map l
-  where ls = lines finale
+villagers :: [(String, String)]
+villagers =
+  [ ("house", "Jack built.")
+  , ("malt", "lay in")
+  , ("rat", "ate")
+  , ("cat", "killed")
+  , ("dog", "worried")
+  , ("cow with the crumpled horn", "tossed")
+  , ("maiden all forlorn", "milked")
+  , ("man all tattered and torn", "kissed")
+  , ("priest all shaven and shorn", "married")
+  , ("rooster that crowed in the morn", "woke")
+  , ("farmer sowing his corn", "kept")
+  , ("horse and the hound and the horn", "belonged to")
+  ]
+
+verse' :: Int -> String
+verse' (-1) = "\n"
+verse' n = " the " ++ who ++ that ++ did ++ verse' (n - 1)
+  where
+    (who, did) = villagers !! n
+    that = if who == "house" then " that " else "\nthat " -- avert your eyes, children!
+
+verse :: Int -> String
+verse n = "This is" ++ verse' n
+
+rhyme = init $ unlines $ map verse [0..11]
