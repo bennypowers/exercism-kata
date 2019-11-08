a4f04dff6f1fa1b9e4f16ce2356d12af340ad463
Benny Powers <web@bennypowers.com>
2019-11-08 12:41:40 +0200

    update python hamming impl

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -10,14 +10,12 @@ def match((fst, snd)):
     """Predicate of a Pair - are the members equal?"""
     return 1 if fst != snd else 0
 
-def zip_chars(string_a, string_b):
-    """Zips the characters in two strings"""
-    return zip(list(string_a), list(string_b))
+to_matches = partial(map, match)
+
+sum_matches = compose(sum, to_matches)
 
 def distance(strand_a, strand_b):
     """Computes the distance between two strands"""
-    to_matches = partial(map, match)
-    sum_matches = compose(sum, to_matches)
     if len(strand_a) != len(strand_b):
         raise ValueError("Strands are of different length")
-    return sum_matches(zip_chars(strand_a, strand_b))
+    return sum_matches(zip(strand_a, strand_b))
