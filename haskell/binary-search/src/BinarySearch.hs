a6c109c5dfb3d63ba2edacebb09fe7c39f5f1e40
Benny Powers <web@bennypowers.com>
2019-12-05 17:38:55 +0200

    haskell: binary-search: passes tests

---
+++ haskell/binary-search/src/BinarySearch.hs
@@ -0,0 +1,19 @@
+module BinarySearch (find) where
+
+import           Data.Array
+
+findInRange :: (Show a, Ord a) => (Int, Int) -> Array Int a -> a -> Maybe Int
+findInRange (lower, upper) arr x
+  | null arr         = Nothing
+  | x == arr ! upper = Just upper
+  | x == arr ! lower = Just lower
+  | x == arr ! pivot = Just pivot
+  | lower == pivot   = Nothing
+  | upper == pivot   = Nothing
+  | x <  arr ! pivot = findInRange (lower, pivot - 1) arr x
+  | x >  arr ! pivot = findInRange (pivot, upper - 1) arr x
+  | otherwise        = Nothing
+  where pivot = lower + ((upper - lower) `div` 2)
+
+find :: (Show a, Ord a) => Array Int a -> a -> Maybe Int
+find arr = findInRange (bounds arr) arr
