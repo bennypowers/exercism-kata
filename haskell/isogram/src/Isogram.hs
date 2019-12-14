fd408e05055a67259c56b0b6bcedbe18c64e0979
Benny Powers <web@bennypowers.com>
2019-12-14 19:37:17 +0200

    haskell: isogram: refactor

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,27 +1,8 @@
 module Isogram (isIsogram) where
 
-import Data.List (elemIndex, isInfixOf)
-import Data.Char (isAlpha, toLower)
-import Data.Maybe (fromMaybe)
-
-lowercase :: String -> String
-lowercase = map toLower
-
-alphas :: String -> String
-alphas = filter isAlpha
-
-charOnce :: Char -> String -> Bool
-charOnce x xs = not $ isInfixOf [x] (isolate x xs)
-
-isolate :: Char -> String -> String
-isolate x xs = pref ++ suff
-  where
-    i = elemIndex x xs
-    suff = drop (fromMaybe (-1) i + 1) xs
-    pref = take (fromMaybe (-1) i) xs
+import           Data.Char (isAlpha, toLower)
+import           Data.List (group, sort)
 
 isIsogram :: String -> Bool
-isIsogram string = all isUnique filtered
-  where
-    filtered = alphas . lowercase $ string
-    isUnique = (`charOnce` filtered)
+isIsogram = all (== 1) . fmap length . group . sort . normalizeString
+  where normalizeString = fmap toLower . filter isAlpha
